be9224b6c6716da7fbeb9f4dc16d6f7a43a06230
atikahadzman <atikah.adzman@gmail.com>
2026-06-02 02:10:32 +0800

    added footer

---
+++ src/Footer.jsx
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from "react";
+
+export default function Footer() {
+    return (
+        <span className="text-xs font-bold">
+            Built and designed by Atikah Adzman.
+            <p>All rights reserved. </p>
+        </span>
+    );
+}
