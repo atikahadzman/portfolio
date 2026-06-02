6c1dea4118221092ebd3bdafb5fe3f1a11f7e360
atikahadzman <atikah.adzman@gmail.com>
2026-06-02 13:30:39 +0800

    show social icon under bio

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 
 export default function Banner() {
     const urlEmail = import.meta.env.VITE_URL_EMAIL;
+    const urlLinkedin = import.meta.env.VITE_URL_LINKEDIN;
+    const urlGithub = import.meta.env.VITE_URL_GITHUB;
     const appName = import.meta.env.VITE_APP_NAME;
 
     return (
@@ -34,6 +36,33 @@ export default function Banner() {
                         className="font-poppins border-2 border-white text-white px-6 py-3 hover:bg-white hover:text-blue-600 transition"
                     >
                         Send Email
+                    </a>
+                </div>
+                <div className="flex items-center justify-center md:justify-start gap-2 pt-4">
+                    <a href={urlGithub} target="_blank" rel="noopener noreferrer"
+                        className="md:hidden px-6 text-sm font-medium transition-all duration-200 relative
+                            after:absolute after:bottom-0 after:left-0 after:h-[2px] after:bg-white
+                            after:transition-all after:duration-200 text-white hover:translate-x-1 transition-transform hover:text-white/70 after:w-0 hover:after:w-full"
+                    >
+                        <img
+                            src="/github-icon.png"
+                            alt="It's Me!"
+                            className="h-5 w-5 md:h-5 md:w-5"
+                            loading="lazy"
+                        />
+                    </a>
+
+                    <a href={urlLinkedin} target="_blank" rel="noopener noreferrer"
+                        className="md:hidden px-6 text-sm font-medium transition-all duration-200 relative
+                            after:absolute after:bottom-0 after:left-0 after:h-[2px] after:bg-white
+                            after:transition-all after:duration-200 text-white hover:translate-x-1 transition-transform hover:text-white/70 after:w-0 hover:after:w-full"
+                    >
+                        <img
+                            src="/linkedin-icon.png"
+                            alt="It's Me!"
+                            className="h-5 w-5 md:h-5 md:w-5"
+                            loading="lazy"
+                        />
                     </a>
                 </div>
             </div>
